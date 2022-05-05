8f66046639433efaebeeaafeb34d8a65b9100e8f
Carlos Eduardo da Trindade Alfaro <101533861+cadualfaro@users.noreply.github.com>
2022-05-05 19:03:26 -0300

    Update Objetos5.java

--- Objetos5.java
+++ Objetos5.java
@@ -1,8 +1,3 @@
-/*
-Implemente o método depositar na classe Conta. Recebe um valor como parâmetro e adiciona no saldo
-Implemente o método getNomeDono(), que retorna o nome do dono da Conta.
-Implemente a função transferir. Parâmetros: Conta origem, Conta dest, float valor
- */
 package objetos5;
 
 import java.util.Random;
